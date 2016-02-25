82344009fbff7df696fc20fc3bbe49a233b91e3b
Krzysztof <kris@m4400.krzwski>
2016-02-25 22:44:23 +0100

    s00e01

---
+++ app/src/main/java/com/example/kris/webbook/TutorialActivity.java
@@ -0,0 +1,10 @@
+package com.example.kris.webbook;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kris on 25.02.16.
+ */
+public class TutorialActivity extends AppCompatActivity {
+}
