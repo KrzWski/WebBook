520e632e1bd664a7df47ecd61619ce6e5bd76d3b
kris <esbits@gmail.com>
2016-02-25 23:52:32 +0100

    s00e05 ok

--- app/src/main/java/com/example/kris/webbook/TutorialActivity.java
+++ app/src/main/java/com/example/kris/webbook/TutorialActivity.java
@@ -1,10 +1,25 @@
 package com.example.kris.webbook;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by kris on 25.02.16.
  */
 public class TutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_tutorial);
+    }
+
+    public void onNextClick(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
 }
